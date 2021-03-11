e41e4a18162ef42a6676ad904241241c3496e173
Dave <pzhang65@uwo.ca>
2021-03-10 23:43:46 -0500

    Modifying base

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 #src/models/__init__.py
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 db = declarative_base()

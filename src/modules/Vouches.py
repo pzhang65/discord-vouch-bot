ca5efee5998b9995c635c5bc416ce37705204b95
Dave <pzhang65@uwo.ca>
2021-03-11 23:57:35 -0500

    adding vouch join table

--- src/modules/Vouches.py
+++ src/modules/Vouches.py
@@ -1,15 +1,24 @@
-#src/modules/Vouch.py
+#src/modules/Vouches.py
 import discord
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class Vouches:
     format = 'Please follow this format: $vouch @user positive/negative'
 
-    def __init__(self, user: discord.User, target: discord.User, positive: bool):
+    giver = Column(String(128), primary_key=True, nullable=False)
+    receiver = Column(String(128), nullable=False, default=0)
+    #vouchers = Column(String(128), nullable=True)
+
+    def __init__(self, give: str, receiver: str, positive: bool):
         '''
         Class constructor
         '''
-        self.user = user
-        self.target = target
+        self.giver = giver
+        self.receiver = receiver
         self.positive = positive
 
     def save(self):

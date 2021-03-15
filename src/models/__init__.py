ed702096d06185ee76e9160278723e7be5126016
Dave <pzhang65@uwo.ca>
2021-03-14 22:37:13 -0400

    Fixing base declaration and inheritance

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,8 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///vouches.db')
+engine = create_engine('sqlite:///test.db')
 Session = sessionmaker(bind=engine)
 
-Base = declarative_base()
+Base = declarative_base() # Declare Base for all models to inherit from
+
+# All models imported back into init for Base to find
+from .Vouches import Vouches
+from .User import User
+
+# Base can now find all classes inherited from it
 Base.metadata.create_all(engine)
